1c66cbd6c1931232224de6ef82bda62d8284b726
Johnny Sung <johnny@Johnnyteki-MacBook-Pro.local>
2012-03-11 02:33:25 +0800

    ASIHTTPRequest lbrary

--- HttpRequestTest2/ViewController.h
+++ HttpRequestTest2/ViewController.h
@@ -2,12 +2,24 @@
 //  ViewController.h
 //  HttpRequestTest2
 //
-//  Created by Johnny on 12/3/11.
+//  Created by Johnny on 12/3/9.
 //  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import "ASIHTTPRequest.h"
 
 @interface ViewController : UIViewController
+
+
+
+
+- (IBAction)grabURLInBackground:(id)sender;
+
+- (void)requestFinished:(ASIHTTPRequest *)request;
+
+- (void)requestFailed:(ASIHTTPRequest *)request;
+- (IBAction)request_btn_onclick:(id)sender;
+@property (retain, nonatomic) IBOutlet UILabel *response_label;
 
 @end
--- HttpRequestTest2/ViewController.m
+++ HttpRequestTest2/ViewController.m
@@ -2,13 +2,14 @@
 //  ViewController.m
 //  HttpRequestTest2
 //
-//  Created by Johnny on 12/3/11.
+//  Created by Johnny on 12/3/9.
 //  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
 //
 
 #import "ViewController.h"
 
 @implementation ViewController
+@synthesize response_label;
 
 - (void)didReceiveMemoryWarning
 {
@@ -26,6 +27,7 @@
 
 - (void)viewDidUnload
 {
+    [self setResponse_label:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -57,4 +59,43 @@
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
+#pragma mark - ASIHTTPRequest area
+
+- (IBAction)grabURLInBackground:(id)sender
+{
+    NSURL *url = [NSURL URLWithString:@"http://127.0.0.1/httptest/t.php"];
+    ASIHTTPRequest *request = [ASIHTTPRequest requestWithURL:url];
+    [request setDelegate:self];
+    [request startAsynchronous];
+}
+
+- (void)requestFinished:(ASIHTTPRequest *)request
+{
+    // Use when fetching text data
+    NSString *responseString = [request responseString];
+    
+    response_label.text=responseString;
+    // Use when fetching binary data
+//    NSData *responseData = [request responseData];
+}
+
+- (void)requestFailed:(ASIHTTPRequest *)request
+{
+    NSError *error = [request error];
+    // inspect error
+    NSLog(@"%@", [error localizedDescription]);
+}
+
+- (IBAction)request_btn_onclick:(id)sender {
+    
+    [self grabURLInBackground:sender];
+}
+
+
+
+
+- (void)dealloc {
+    [response_label release];
+    [super dealloc];
+}
 @end
